0861ea0b2b962619dd5d89dadcc1b22cbda9dc5c
runt1m33rr0r <19938633+runt1m33rr0r@users.noreply.github.com>
2017-09-23 18:11:10 +0300

    adding teachers finished

--- server/user/user.controller.js
+++ server/user/user.controller.js
@@ -6,7 +6,57 @@ function init({
 }) {
     const {
         UserData,
+        TeacherData,
+        GroupData,
+        SubjectData,
     } = data;
+
+    function registerTeacher(
+        firstName,
+        lastName,
+        username,
+        isLead,
+        group,
+        subjects) {
+        if (isLead) {
+            return GroupData.getGroupByName(group)
+                .then((result) => {
+                    if (result) {
+                        return SubjectData.getSubjectsByCodes(subjects);
+                    } else {
+                        return Promise.reject({
+                            message: 'Невалидна група!',
+                        });
+                    }
+                })
+                .then((subjectCodes) => {
+                    return TeacherData.createTeacher(
+                        firstName,
+                        lastName,
+                        username,
+                        true,
+                        group,
+                        subjectCodes,
+                    );
+                });
+        } else {
+            return SubjectData.getSubjectsByCodes(subjects)
+                .then((subjectCodes) => {
+                    return TeacherData.createTeacher(
+                        firstName,
+                        lastName,
+                        username,
+                        false,
+                        '',
+                        subjectCodes,
+                    );
+                });
+        }
+    }
+
+    function registerStudent(firstName, lastName, username, group) {
+
+    }
 
     function getUserByUsername(req, res) {
         const username = req.query.username;
@@ -78,17 +128,50 @@ function init({
 
             const username = req.body.username;
             const password = req.body.password;
+            const firstName = req.body.firstName;
+            const lastName = req.body.lastName;
             const salt = encryption.getSalt();
             const hash = encryption.getHash(salt, password);
+            const leadTeacher = req.body.leadTeacher;
+            const group = req.body.group;
+            const subjects = req.body.subjects;
+            const userType = req.body.userType;
             const roles = [roleTypes.Normal];
 
-            UserData.createUser(username, roles, salt, hash)
-                .then(() => {
-                    res.redirect('/users/login');
-                })
-                .catch((err) => res.render('base/error', {
-                    error: err,
-                }));
+            if (typeof leadTeacher !== 'boolean') {
+                return res.status(500).json({
+                    message: 'Невалиден потребител!',
+                });
+            }
+
+            if (userType === roleTypes.Student) {
+                registerStudent(firstName, lastName, username, group);
+            } else if (userType === roleTypes.Teacher) {
+                registerTeacher(
+                        firstName,
+                        lastName,
+                        username,
+                        leadTeacher,
+                        group,
+                        subjects)
+                    .then(() => {
+                        return UserData.createUser(username, roles, salt, hash);
+                    })
+                    .then(() => {
+                        res.status(200).json({
+                            message: 'cool',
+                        });
+                    })
+                    .catch((err) => {
+                        res.status(500).json({
+                            message: err.message,
+                        });
+                    });
+            } else {
+                res.status(500).json({
+                    message: 'Невалиден потребител!',
+                });
+            }
         },
         getUnauthorized(req, res) {
             res.render('base/unauthorized');
