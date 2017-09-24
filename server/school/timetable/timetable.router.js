94cfcf262832886b4805665490ac7cd399f032c4
nutrinobomber <neutrinobomber@users.noreply.github.com>
2017-09-24 22:15:34 +0300

    some user restrictions

--- server/school/timetable/timetable.router.js
+++ server/school/timetable/timetable.router.js
@@ -1,6 +1,9 @@
+const roleTypes = require('../../utils/roletypes');
+
 function init({
     app,
     controllers,
+    middlewares,
 }) {
     const settingsRoute = '/school/settings/';
     const controller = controllers.timetable;
@@ -8,29 +11,58 @@ function init({
     app.get('/api/groups', controller.getAllGroups);
     app.get('/api/subjects', controller.getAllSubjects);
 
-    app.get(settingsRoute + 'base', controller.getBaseSettingsPage);
-    app.get(settingsRoute + 'subjects', controller.getSubjectSettingsPage);
-    app.get(settingsRoute + 'groups', controller.getGroupsSettingsPage);
-    app.get(settingsRoute + 'timetable', controller.getTimetableSettingsPage);
+    app.get(
+        settingsRoute + 'base',
+        middlewares.user.isInRole(roleTypes.Teacher),
+        controller.getBaseSettingsPage);
+    app.get(
+        settingsRoute + 'subjects',
+        middlewares.user.isInRole(roleTypes.Teacher),
+        controller.getSubjectSettingsPage);
+    app.get(
+        settingsRoute + 'groups',
+        middlewares.user.isInRole(roleTypes.Teacher),
+        controller.getGroupsSettingsPage);
+    app.get(
+        settingsRoute + 'timetable',
+        middlewares.user.isInRole(roleTypes.Teacher),
+        controller.getTimetableSettingsPage);
     app.get(
         settingsRoute + 'timetable/generate',
+        middlewares.user.isInRole(roleTypes.Teacher),
         controller.getGenerateTimetablePage);
     app.get(
         settingsRoute + 'timetable/create',
+        middlewares.user.isInRole(roleTypes.Teacher),
         controller.getCreateTimetablePage);
 
-    app.post(settingsRoute + 'base', controller.saveBaseSettings);
-    app.post(settingsRoute + 'subjects', controller.saveSubjectSettings);
-    app.post(settingsRoute + 'groups', controller.saveGroupsSettings);
-    app.post(settingsRoute + 'timetable', controller.saveTimetableSettings);
+    app.post(
+        settingsRoute + 'base',
+        middlewares.user.isInRole(roleTypes.Teacher),
+        controller.saveBaseSettings);
+    app.post(
+        settingsRoute + 'subjects',
+        middlewares.user.isInRole(roleTypes.Teacher),
+        controller.saveSubjectSettings);
+    app.post(
+        settingsRoute + 'groups',
+        middlewares.user.isInRole(roleTypes.Teacher),
+        controller.saveGroupsSettings);
+    app.post(
+        settingsRoute + 'timetable',
+        middlewares.user.isInRole(roleTypes.Teacher),
+        controller.saveTimetableSettings);
     app.post(
         settingsRoute + 'timetable/generate',
+        middlewares.user.isInRole(roleTypes.Teacher),
         controller.generateTimetable);
     app.post(
         settingsRoute + 'timetable/delete',
+        middlewares.user.isInRole(roleTypes.Teacher),
         controller.deleteTimetable);
     app.post(
         settingsRoute + 'timetable/add',
+        middlewares.user.isInRole(roleTypes.Teacher),
         controller.createLesson);
 }
 
