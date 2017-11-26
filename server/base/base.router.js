c9c2d0c889cf6dfda25519a74a50d3d53d9befa9
runt1m33rr0r <19938633+runt1m33rr0r@users.noreply.github.com>
2017-11-26 20:57:38 +0200

    removed views. Initial REST transformation

--- server/base/base.router.js
+++
@@ -1,7 +0,0 @@
-function init({ app, controllers }) {
-  app.get('/', controllers.base.redirectHome);
-  app.get('/home', controllers.base.getHome);
-  app.get('/help', controllers.base.getHelp);
-}
-
-module.exports = { init };
--- server/config/app/app.js
+++ server/config/app/app.js
@@ -1,34 +1,11 @@
-const path = require('path');
 const bodyParser = require('body-parser');
 const express = require('express');
 const expressSession = require('express-session');
 const MongoStore = require('connect-mongo')(expressSession);
 const settings = require('../settings');
 
-function setupStatics(app) {
-  const bootstrapPath = path.join(__dirname, '../../../node_modules/bootstrap');
-  const fontAwesome = path.join(__dirname, '../../../node_modules/font-awesome');
-  const jqueryPath = path.join(__dirname, '../../../node_modules/jquery');
-  const popperPath = path.join(__dirname, '../../../node_modules/popper.js');
-  const momentPath = path.join(__dirname, '../../../node_modules/moment');
-  const staticsPath = path.join(__dirname, '../../../public');
-
-  app.use('/public', express.static(jqueryPath));
-  app.use('/public', express.static(popperPath));
-  app.use('/public', express.static(bootstrapPath));
-  app.use('/public', express.static(fontAwesome));
-  app.use('/public', express.static(momentPath));
-
-  app.use('/public', express.static(staticsPath));
-}
-
 function init(db) {
   const app = express();
-
-  app.set('view engine', 'pug');
-  app.set('views', path.join(__dirname, '../../../server/views'));
-
-  setupStatics(app);
 
   app.use(bodyParser.urlencoded({
     extended: true,
