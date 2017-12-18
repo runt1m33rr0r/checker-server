78fdaecc35432dd7e7657d2a7fb3ce6cc65debc0
runt1m33rr0r <19938633+runt1m33rr0r@users.noreply.github.com>
2017-12-18 23:00:05 +0200

    use https for pinging

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-const http = require('http');
+const https = require('https');
 
 // keep heroku from putting our server to sleep
 setInterval(() => {
-  http.get('https://school-system-server.herokuapp.com');
+  try {
+    https.get('https://school-system-server.herokuapp.com/');
+  } catch (e) {
+    console.log(e.message);
+  }
 }, 300000); // ping it every 5 minutes (300000)
 
 require('./server/config').init();
