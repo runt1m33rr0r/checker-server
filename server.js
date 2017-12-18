be23379692a5e0d0a3b271496c6db3b96dff966b
runt1m33rr0r <19938633+runt1m33rr0r@users.noreply.github.com>
2017-12-18 22:14:46 +0200

    keep heroku from putting the server to sleep

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
+const http = require('http');
+
+// keep heroku from putting our server to sleep
+setInterval(() => {
+  http.get('https://school-system-server.herokuapp.com');
+}, 300000); // ping it every 5 minutes (300000)
+
 require('./server/config').init();
