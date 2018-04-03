c3e89f53ad7e222273ce5d225f025020b2469226
runt1m33rr0r <19938633+runt1m33rr0r@users.noreply.github.com>
2018-04-03 20:38:20 +0300

    fixed router crash

--- server/school/student/student.router.js
+++ server/school/student/student.router.js
@@ -3,16 +3,6 @@ const roles = require('../../utils/roletypes');
 function init({ app, controllers, middlewares }) {
   const controller = controllers.student;
 
-  app.get(
-    '/students/timetable',
-    middlewares.user.isInRole(roles.Student),
-    controller.getTimetablePage,
-  );
-  app.get(
-    '/students/absences',
-    middlewares.user.isInRole(roles.Student),
-    controller.getAbsencesPage,
-  );
   app.post('/students/check', middlewares.user.isInRole(roles.Normal), controller.createEncoding);
 }
 
