5f323a22548e95fd443834196ebee3941f616ac8
runt1m33rr0r <19938633+runt1m33rr0r@users.noreply.github.com>
2018-05-07 18:13:31 +0300

    added validations to the lesson data

--- server/school/lesson/lesson.data.js
+++ server/school/lesson/lesson.data.js
@@ -1,43 +1,69 @@
 const BaseData = require('../../base/base.data');
+const { validateString, validateObject, validateArray } = require('../../utils/validators');
 
 class LessonData extends BaseData {
-  async checkUniqueLesson(groupName, subjectCode, teacherUsername, timeslot) {
+  _validateLessonData(groupName, subjectCode, teacherUsername, timeslot) {
+    validateString({ input: groupName, errorMessage: 'Липва име на група!' });
+    validateString({ input: subjectCode, errorMessage: 'Липсва код на предмет!' });
+    validateString({ input: teacherUsername, errorMessage: 'Липва име преподавател!' });
+    validateObject({ input: timeslot, errorMessage: 'Липсва час!' });
+  }
+
+  async _checkUniqueLesson(groupName, subjectCode, teacherUsername, timeslot) {
+    this._validateLessonData(groupName, subjectCode, teacherUsername, timeslot);
+
     const lesson = await this.collection.findOne({
       groupName,
       subjectCode,
       teacherUsername,
       timeslot,
     });
+
     if (lesson) {
       throw new Error('Такъв час вече съществува!');
     }
   }
 
-  async checkFreeTimeslot(groupName, timeslot) {
-    if (await this.collection.findOne({ groupName, timeslot })) {
+  async _checkFreeTimeslot(groupName, timeslot) {
+    validateString({ input: groupName, errorMessage: 'Липва име на група!' });
+    validateObject({ input: timeslot, errorMessage: 'Липсва час!' });
+
+    const lesson = await this.collection.findOne({
+      groupName,
+      timeslot,
+    });
+
+    if (lesson) {
       throw new Error('Това време е заето!');
     }
   }
 
-  async checkLesson(lesson) {
-    return Promise.all([
-      this.checkUniqueLesson(lesson.group, lesson.subject, lesson.teacher, lesson.timeslot),
-      this.checkFreeTimeslot(lesson.group, lesson.timeslot),
-    ]);
+  async _checkLesson(lesson) {
+    validateObject({ input: lesson, errorMessage: 'Липсва урок!' });
+    const {
+      groupName, subjectCode, teacherUsername, timeslot,
+    } = lesson;
+
+    await this._checkUniqueLesson(groupName, subjectCode, teacherUsername, timeslot);
+    await this._checkFreeTimeslot(groupName, timeslot);
   }
 
   async createLessons(lessons) {
-    if (!Array.isArray(lessons)) {
-      throw new Error('Невалидни уроци!');
-    }
+    validateArray({ input: lessons, errorMessage: 'Липсват уроци!' });
 
     const { Lesson } = this.models;
     const checks = [];
     const lessonModels = [];
 
     for (const lesson of lessons) {
-      lessonModels.push(new Lesson(lesson.group, lesson.subject, lesson.teacher, lesson.timeslot));
-      checks.push(this.checkLesson(lesson));
+      const model = new Lesson(
+        lesson.groupName,
+        lesson.subjectCode,
+        lesson.teacherUsername,
+        lesson.timeslot,
+      );
+      lessonModels.push(model);
+      checks.push(this._checkLesson(lesson));
     }
 
     await Promise.all(checks);
@@ -45,7 +71,9 @@ class LessonData extends BaseData {
   }
 
   async createLesson(groupName, subjectCode, teacherUsername, timeslot) {
-    await this.checkLesson({
+    this._validateLessonData(groupName, subjectCode, teacherUsername, timeslot);
+
+    await this._checkLesson({
       groupName,
       subjectCode,
       teacherUsername,
@@ -58,28 +86,27 @@ class LessonData extends BaseData {
   }
 
   async getLessonsByGroupName(groupName) {
+    validateString({ input: groupName, errorMessage: 'Липва име на група!' });
+
     return this.collection.find({ groupName }).toArray();
   }
 
   async getLessonsByTeacher(username) {
+    validateString({ input: username, errorMessage: 'Липсва потребителско име!' });
+
     return this.collection.find({ teacherUsername: username }).toArray();
   }
 
   async deleteLesson({
     groupName, subjectCode, teacherUsername, timeslot,
   }) {
-    const {
-      fromHour, fromMinute, toHour, toMinute, day,
-    } = timeslot;
+    this._validateLessonData(groupName, subjectCode, teacherUsername, timeslot);
+
     return this.deleteOne({
       groupName,
       subjectCode,
       teacherUsername,
-      'timeslot.fromHour': fromHour,
-      'timeslot.fromMinute': fromMinute,
-      'timeslot.toHour': toHour,
-      'timeslot.toMinute': toMinute,
-      'timeslot.day': day,
+      timeslot,
     });
   }
 }
