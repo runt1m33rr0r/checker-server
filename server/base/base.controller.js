24431c18a58c807f3a9ed23480a93f9aec21b8fb
runt1m33rr0r <19938633+runt1m33rr0r@users.noreply.github.com>
2018-05-07 22:42:06 +0300

    include date and time in home message

--- server/base/base.controller.js
+++ server/base/base.controller.js
@@ -1,6 +1,9 @@
 const init = () => ({
   async getHome(req, res) {
-    return res.json({ success: 'true', message: 'Hello!' });
+    return res.json({
+      success: 'true',
+      message: `Hello! The current date and time are ${new Date()}`,
+    });
   },
 });
 
